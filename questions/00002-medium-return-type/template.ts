1c052999b2de43318726c97526ac747b660caa09
raotaohub <1060791388@qq.com>
2022-06-16 22:29:50 +0800

    example:00002

--- questions/00002-medium-return-type/template.ts
+++ questions/00002-medium-return-type/template.ts
@@ -1,1 +1,3 @@
-type MyReturnType<T> = any
+// 1. T 的类型约束为 函数，形参和返回值为any
+// 2. 若 T 满足形参any，且推导(infer)存在返回值 R 那么返回 R的类型，否则返回never
+type MyReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : never
